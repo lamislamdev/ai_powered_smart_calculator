5f29e8acf386c385c51a131aa1dcaab0877ce254
lam islam <lamislam4444@gmail.com>
2024-12-22 01:17:36 +0600

    Sum Bug Fix

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('component')
+    @include('components.drawPad')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('pages.home');
 });
 
